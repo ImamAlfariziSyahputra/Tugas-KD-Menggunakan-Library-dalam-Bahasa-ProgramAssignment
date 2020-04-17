9a60b7cfabb17056d02d3ba694cae6c2bb974f62
Imam Alfarizi Syahputra <63343105+ImamAlfariziSyahputra@users.noreply.github.com>
2020-04-17 21:04:12 +0700

    Membuat Aplikasi Menggunakan Library

---
+++ connection.php
@@ -0,0 +1,22 @@
+<?php 
+    Class dbObj {
+        var $servername = "localhost";
+        var $username = "root";
+        var $password = "";
+        var $dbname = "db_test";
+        var $conn;
+
+        function getConnstring() {
+            $con = mysqli_connect($this->servername,$this->username,$this->password,$this->dbname) or
+            die("Connection failed : " . mysqli_connect_error());
+            // Check COnnection
+            if (mysqli_connect_errno()) {
+                printf("Connection failed: %s\n", mysqli_connect_error());
+                exit();
+            }else {
+                $this->conn = $con;
+            }
+            return $this->conn;
+        }
+    }
+?>
